f08dc27e3ef9a125786fab7f73d0a1f9a232aff2
Janetdelavan <delaj779@newschool.edu>
2022-04-02 19:21:27 -0400

    week4

---
+++ project3.js
@@ -0,0 +1,24 @@
+console.log('Hi');
+
+fetch('https://api.airtable.com/v0/appHeSkxwjOR0q5KH/fauvism',{
+    headers: {
+        Authorization: 'Bearer keyF2Ys0wHSHQ5EfY',
+    },
+})
+    .then (response => response.json())
+    .then (data => {
+
+
+        console.log(data);
+
+        const container = document.querySelector ('.container');
+        data.records.forEach(album => {
+            console.log(album);
+            container.innerHTML += `
+                <div class="album">
+                    <h3> ${album.fields.artist}</h3>
+                    <h5> ${album.fields.title}</h5>
+                </div>
+                `;
+        });
+    });
---
+++ week4.js
@@ -0,0 +1,32 @@
+var images= [
+    "https://qg1pwcso2l-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/passenger_interior-2.jpg",
+    "https://qg1pwcso2l-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/driver_front_34.jpg",
+    "https://qg1pwcso2l-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/driver-dash.jpg",
+    "https://qg1pwcso2l-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/passenger_dash-1.jpg",
+    "https://qg1pwcso2l-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/speedi.jpg",
+    "https://qg1pwcso2l-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/driver_rear_34-1.jpg",
+    "https://qg1pwcso2l-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/front-2.jpg"]
+
+    var num=0;//num is a variable to hold the current image
+
+function next()
+{
+  var slider =document.getElementById('slider');
+  num++;
+  if(num>=images.length)
+  {
+    num=0;
+  }
+  slider.src=images[num];
+}
+
+function previous()
+{
+  var slider =document.getElementById('slider');
+  num--;
+  if(num<0)
+  {
+    num=images.length-1;
+  }
+  slider.src=images[num];
+}
